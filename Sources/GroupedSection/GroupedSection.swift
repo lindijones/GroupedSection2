6999d3f7523031ad4d7c83e7bda959ec57850667
Thomas Linder <thomas.l.linder@daimler.com>
2019-09-25 17:01:07 +0200

    First commit

--- Sources/GroupedSection/GroupedSection.swift
+++ Sources/GroupedSection/GroupedSection.swift
@@ -1,3 +1,11 @@
-struct GroupedSection {
-    var text = "Hello, World!"
+struct GroupedSection<Identifier: Hashable, Row> {
+    
+    var identifier : Identifier
+    var rows : [Row]
+    
+    static func group(_ rows : [Row], by identifierForRow: (Row) -> Identifier) -> [GroupedSection<Identifier, Row>] {
+        let groupedHeadlines = Dictionary(grouping: rows, by: identifierForRow)
+        return groupedHeadlines.map(GroupedSection.init)
+    }
+    
 }
