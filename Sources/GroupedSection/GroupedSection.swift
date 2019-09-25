3b31267ab88fd7279ad52c576fa97af914842bff
Thomas Linder <thomas.l.linder@daimler.com>
2019-09-25 17:12:44 +0200

    Second Fix

--- Sources/GroupedSection/GroupedSection.swift
+++ Sources/GroupedSection/GroupedSection.swift
@@ -1,9 +1,9 @@
 public struct GroupedSection<Identifier: Hashable, Row> {
     
-    var identifier : Identifier
-    var rows : [Row]
+    public let identifier : Identifier
+    public let rows : [Row]
     
-    static func group(_ rows : [Row], by identifierForRow: (Row) -> Identifier) -> [GroupedSection<Identifier, Row>] {
+    public static func group(_ rows : [Row], by identifierForRow: (Row) -> Identifier) -> [GroupedSection<Identifier, Row>] {
         let groupedHeadlines = Dictionary(grouping: rows, by: identifierForRow)
         return groupedHeadlines.map(GroupedSection.init)
     }
